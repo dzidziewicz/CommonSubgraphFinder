cb2fe5db1b2cc1015310f46a228b27008f50ac2f
adgryz <acros787@gmail.com>
2018-11-19 22:49:29 +0100

    Examples

--- CommonSubgraphFinder/Program.cs
+++ CommonSubgraphFinder/Program.cs
@@ -11,8 +11,8 @@ namespace CommonSubgraphFinder
 {
     internal class Program
     {
-        const string FileName1 = "7_10_A_Drejer.csv";
-        const string FileName2 = "7_10_B_Drejer.csv";
+        const string FileName1 = "9_12_A_Drejer.csv";
+        const string FileName2 = "9_12_B_Drejer.csv";
         const bool CountVerticesOnly = true;
         const bool UseExactAlgorithm = true;
         private static void Main(string[] args)
